cce63f5242eed3ff8f6b9fc0385b8ae53e6d8f34
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-13 16:02:40 -0500

    Write pseudocode about getting input from btns
    Serves as a starting point.

--- calculator.js
+++ calculator.js
@@ -13,3 +13,16 @@ const divide = (a, b) => a / b;
 function operate(a, b, operator) {
 
 };
+
+// GOAL: Take the input from the user pressing the buttons and have it 
+// appear in display
+/*
+*   MAKE a reference to display
+*   MAKE a reference to all buttons
+*   USING an event listener
+*       ADD the correct number to display when it is pressed
+*       (Possibly make a node list, use forEach to cycle through it.
+*       and return it's text content so that separate event listeners
+*        do not have to be made for each number button.
+*  STORE display content somewhere...
+*/
