be08ab901d0eacfaaf4e4a65d99aae5755d6b120
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-21 16:35:51 -0500

    Add conditional to op event listener
    The conditions that were used prevent the user from inputting an
    operator before giving numOne a value and prevents them from inputting
    more than one operators.

--- calculator.js
+++ calculator.js
@@ -56,10 +56,12 @@ const operators = document.querySelectorAll(".operator");
 
 operators.forEach((op) => {
     op.addEventListener("click", () => {
-        let input = op.outerText;
-        calcPara.textContent += ` ${input} `;
-
-        operator = input;
+        if (numOne && operator == undefined) {
+            let input = op.outerText;
+            calcPara.textContent += ` ${input} `;
+    
+            operator = input;
+        };
     });
 });
 
