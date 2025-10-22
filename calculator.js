a4c80a04e724dbd089e5f232c51de8a86b5cd6b3
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-22 12:35:27 -0500

    Separate code with if else statement
    Not all of the code inside of getNumbersFromInput needs to run every
    single time that the function is called. Using an if else statement
    makes the code only run when necessary.

--- calculator.js
+++ calculator.js
@@ -114,16 +114,19 @@ equalBtn.addEventListener("click", () => {
 function getNumbersFromInput() {
     let paraArr = calcPara.textContent.split(" ");
 
-    numOne = Number(paraArr[0]);
-
-    numTwo = Number(paraArr[2]);
-
-    let addFourEveryLoop = 0;
-
-    for (let i = 1; i <= amountOperateWasCalled; i++) {
-        addFourEveryLoop += 4;
+    if (amountOperateWasCalled === 0) {
+        numOne = Number(paraArr[0]);
+    
+        numTwo = Number(paraArr[2]);
+    } else {
+        let addFourEveryLoop = 0;
+    
+        for (let i = 1; i <= amountOperateWasCalled; i++) {
+            addFourEveryLoop += 4;
+        };
+    
+        numOne = Number(paraArr[0 + addFourEveryLoop]);
+        numTwo = Number(paraArr[2 + addFourEveryLoop]);
     };
 
-    numOne = Number(paraArr[0 + addFourEveryLoop]);
-    numTwo = Number(paraArr[2 + addFourEveryLoop]);
 };
