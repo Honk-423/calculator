61f09134a39da1922dd88b2f052cef9a3c9ff01d
Talia <139439068+Honk-423@users.noreply.github.com>
2025-10-30 17:39:02 -0500

    Paste code from nums and replace input with e.key
    Paste code from numbers and replace references to input with e.key.
    Gives the keydown event listener just as much functionality as clicking
    the number buttons, making the numbers keyboard supported.

--- calculator.js
+++ calculator.js
@@ -238,7 +238,13 @@ document.addEventListener("keydown", (e) => {
     const validNums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     if (validNums.toString().indexOf(e.key) > -1) {
- 
+        if (!(hasOperateBeenCalled)) {
+            calcPara.textContent += e.key;
+    
+            getNumbersFromInput();
+        } else {
+            additionalNumbers.push(e.key);
+        };
     };
 });
 
